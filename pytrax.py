24baaa142214cb018026eeebdf6441f7eabb8068
Chris McCormick <chris@mccormick.cx>
2016-05-29 09:15:17 +0800

    Only print lengths if instruments, samples, patterns are loaded in demo.

--- pytrax.py
+++ pytrax.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     import pprint
     if len(sys.argv) > 1:
-        info = impulsetracker.parse_file(sys.argv[1], with_instruments=True, with_samples=True, with_patterns=True)
+        info = impulsetracker.parse_file(sys.argv[1], with_samples=True, with_instruments=True, with_patterns=True)
         instruments = info.pop("instruments", None)
         samples = info.pop("samples", None)
         patterns = info.pop("patterns", None)
@@ -12,9 +12,12 @@ if __name__ == "__main__":
         for i in info:
             print i, "=>", info[i]
         print
-        print len(instruments), "instruments"
-        print len(samples), "samples"
-        print len(patterns), "patterns"
+        if instruments:
+            print len(instruments), "instruments"
+        if samples:
+            print len(samples), "samples"
+        if patterns:
+            print len(patterns), "patterns"
     else:
         print "usage:", sys.argv[0], "FILE.it"
 
